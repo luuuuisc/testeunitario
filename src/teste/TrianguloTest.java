1c253481a8f095e2130d4e98d3b8bfc0d412d748
Luiz Claudio <luizclaudio60a@gmail.com>
2024-05-27 15:53:43 -0300

    Alterar metodo auxiliar

--- src/teste/TrianguloTest.java
+++ src/teste/TrianguloTest.java
@@ -1,16 +1,13 @@
 package teste;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 import org.junit.Test;
-
 import entities.Triangulo;
-public class TrianguloTest{
+
+public class TrianguloTest {
 	
 	@Test
-	public void tipoTrianguloShouldReturnEquilateroWhenAllSidesAreEqual() {
-		
+	public void tipoTrianguloShouldReturnEquilateroWhenAllSidesAreEqual() {	
 		int sideA = 2, sideB = 2, sideC = 2;
 		String result = Triangulo.tipoTriangulo(sideA, sideB, sideC);
 		assertEquals("Equilátero", result);
@@ -23,9 +20,6 @@ public class TrianguloTest{
 		assertIsosceles(4, 3, 3);
 	}
 	
-	private void assertIsosceles(int a, int b, int c) {
-		assertEquals("Isósceles", Triangulo.tipoTriangulo(a, b, c));
-	}
 	
 	@Test
 	public void tipoTrianguloShouldReturnEscalenoWhenNoSidesAreEqual() {
@@ -35,28 +29,30 @@ public class TrianguloTest{
 	}
 	
 	
+	@Test
+	public void tipoTrianguloShouldReportErrorWhenSidesDoNotFormTriangle() {
+		assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(1, 1, 3));
+		assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(1, 3, 1));
+		assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(3, 1, 1));
 	
+	}
+	 
+	private void assertIsosceles(int a, int b, int c) {
+		assertEquals("Isósceles", Triangulo.tipoTriangulo(a, b, c));
+	}
+	 
 	
-	public void tipoTrianguloShouldReportErrorWhenAnySideIsZero() {
-	    int sideB = 1, sideC = 1;
+	private void assertTriangleSidesMustBeGreaterThanZero(int a, int b, int c) {
+		assertEquals("Todos os lados devem ser maiores que zero.", Triangulo.tipoTriangulo(a, b, c));
+	}
+	 
+	 
+	public void tipoTrianguloShouldReportErrorWhenAnySideIsZeroOrNegative() {
+		int sideB = 1, sideC = 1;
 
 	    assertTriangleSidesMustBeGreaterThanZero(0, sideB, sideC);
 	    assertTriangleSidesMustBeGreaterThanZero(sideB, 0, sideC);
 	    assertTriangleSidesMustBeGreaterThanZero(sideB, sideC, 0);
 	}
-
-	private void assertTriangleSidesMustBeGreaterThanZero(int a, int b, int c) {
-	    assertEquals("Todos os lados devem ser maiores que zero.", Triangulo.tipoTriangulo(a, b, c));
-	}
-	
-	 @Test
-	    public void tipoTrianguloShouldReportErrorWhenSidesDoNotFormTriangle() {
-	        // Arrange and Act
-	        assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(1, 1, 3));
-	        assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(1, 3, 1));
-	        assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", Triangulo.tipoTriangulo(3, 1, 1));
-	
-	
-	 }
 }
 
